a1081bc65e9180a5a04397ec18dfbd0ee887be87
gammatrue <56622795+gammatrue@users.noreply.github.com>
2023-12-11 10:51:26 +0800

    Update nicegram.js

--- nicegram.js
+++ nicegram.js
@@ -1,5 +1,5 @@
 var modifiedHeaders = $response.headers;
 var modifiedStatus = 'HTTP/1.1 200 OK';
 var body1={"premiumAccess": true};
-
+console.log(modifiedHeaders,modifiedStatus,body1);
 $done({status: modifiedStatus, headers : modifiedHeaders, body : body1});

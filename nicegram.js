e4f9a4fa0a23e5cd66fca7b4573c637493d3b2f3
gammatrue <56622795+gammatrue@users.noreply.github.com>
2023-12-11 10:23:49 +0800

    Update nicegram.js

--- nicegram.js
+++ nicegram.js
@@ -1,6 +1,8 @@
-$response.status=200;
-$response.body={"data": {"premiumAccess": true}};
-const resp ={};
+var resp ={};
 resp.headers =$response.headers;
 resp.body =$response.body;
+resp.status=200;
+console.log(resp.headers);
+console.log(resp.status);
+resp.body={"data": {"premiumAccess": true}};
 $done(resp);
